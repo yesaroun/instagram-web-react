4102ae26f99eccefc13da287f7973b5338d27b64
yesaroun <akor1@naver.com>
2023-02-10 11:25:48 +0900

    로그인 처리

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Profile from "./screens/Profile";
 import SignUp from "./screens/SignUp";
 
 function App() {
+  const isLoggedIn = false;
+
   return (
   <Router>
     <Routes>
-      <Route path="/" element={<Login></Login>}></Route>
-      <Route path="/home" element={<Home/>}></Route>
+      <Route path="/" element={ isLoggedIn ? <Home/> : <Login/>}></Route>
+      {/*<Route path="/home" element={<Home/>}></Route>*/}
       <Route path="/profile" element={<Profile/>}></Route>
-      <Route path="/signup" element={SignUp}></Route>
+      <Route path="/signup" element={isLoggedIn ? null : <SignUp/>}></Route>
     </Routes>
   </Router>
   );
