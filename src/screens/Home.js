2589cc305b37ac66c1286c5247ffeac0cbcef2e5
yesaroun <akor1@naver.com>
2023-02-17 11:52:41 +0900

    feat: feed 부분

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import Avatar from "../components/Avartar";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHeart as SolidHeart} from "@fortawesome/free-solid-svg-icons";
+import {faHeart, faComment, faPaperPlane, faBookmark} from "@fortawesome/free-regular-svg-icons";
 
 const FeedContainer = styled.div`
   display: flex;
@@ -28,9 +31,33 @@ const FeedPhoto = styled.img`
   max-width: 100%;
 `;
 
-const FeedActions = styled.div`
-  display: flex;
+const FeedAction = styled.div`
+  margin-right: 10px;
+  cursor: pointer;
 `;
+
+const FeedWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const FeedActionContainer = styled.div`
+  padding: 12px 15px;
+`;
+
+const Likes = styled.span`
+  font-weight: 600;
+  margin-top: 15px;
+`;
+
 
 function Home() {
   return (
@@ -38,11 +65,28 @@ function Home() {
       <FeedHeader>
         <Avatar
           lg
-          url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgreenaqua.hu%2Fen%2Fhal-sziami-harcoshal-betta-splendens-dumbo.html&psig=AOvVaw2MQ-lkIZ-dEKY_Izsofbf2&ust=1676622423958000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCIiLkbbPmf0CFQAAAAAdAAAAABAD"
+          url="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgreenaqua.hu%2Fen%2Fhal-sziami-harcoshal-betta-splendens-dumbo.html&psig=AOvVaw2MQ-lkIZ-dEKY_Izsofbf2&ust=1676622423958000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCIiLkbbPmf0CFQAAAAAdAAAAABAD"
         />
-        <Nickname>Kickname</Nickname>
+        <Nickname>Nickname</Nickname>
       </FeedHeader>
-      <FeedPhoto src="https://wooarts.com/wp-content/uploads/sites/1/nggallery/amazing-beta-fish/amazing-beta-fish-shots-wooarts-01.jpg"/>
+      <FeedPhoto
+        src="https://wooarts.com/wp-content/uploads/sites/1/nggallery/amazing-beta-fish/amazing-beta-fish-shots-wooarts-01.jpg"/>
+      <FeedActionContainer>
+        <FeedWrapper>
+          <div>
+            <FeedAction>
+              <FontAwesomeIcon style={{color: true ? "tomato" : "inherit"}} icon={true ? SolidHeart : faHeart}/>
+            </FeedAction>
+            <FeedAction>
+              <FontAwesomeIcon size="lg" icon={faComment}/>
+            </FeedAction>
+            <FontAwesomeIcon size="lg" icon={faPaperPlane}/>
+          </div>
+          <div>
+            <FontAwesomeIcon size="lg" icon={faBookmark}/>
+          </div>
+        </FeedWrapper>
+      </FeedActionContainer>
     </FeedContainer>
   );
 }
